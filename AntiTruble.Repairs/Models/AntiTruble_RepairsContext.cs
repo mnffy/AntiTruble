89b0c2c9b9156babb6736eeeacf063d086463496
Sega <alexeevsergey888@gmail.com>
2019-01-22 22:50:01 +0300

    init microservices

---
+++ AntiTruble.Repairs/Models/AntiTruble_RepairsContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AntiTruble.Models
+{
+    public partial class AntiTruble_RepairsContext : DbContext
+    {
+        public AntiTruble_RepairsContext()
+        {
+        }
+
+        public AntiTruble_RepairsContext(DbContextOptions<AntiTruble_RepairsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Repairs> Repairs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+
+            modelBuilder.Entity<Repairs>(entity =>
+            {
+                entity.HasKey(e => e.RepairId);
+
+                entity.Property(e => e.RepairId).ValueGeneratedNever();
+
+                entity.Property(e => e.EndDate).HasColumnType("date");
+
+                entity.Property(e => e.StartDate).HasColumnType("date");
+            });
+        }
+    }
+}
---
+++ AntiTruble.Repairs/Models/Repairs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AntiTruble.Models
+{
+    public partial class Repairs
+    {
+        public long RepairId { get; set; }
+        public byte? RepairType { get; set; }
+        public byte? Status { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public long? Client { get; set; }
+        public long? Master { get; set; }
+    }
+}
